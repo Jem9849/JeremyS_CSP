2188e1ba2cb619df94fc3ec2c9393bb13c231218
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-10-26 11:53:26 -0600

    Added folders to put all my items into. Also added a gitignore.

---
+++ JeremyS_CSP/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JeremyS_CSP
+//
+//  Created by Saysanasy, Jeremy on 10/26/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
