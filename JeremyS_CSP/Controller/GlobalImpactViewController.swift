f0f6d1b4b665409ee710e50d6de8ce05a49fb2fc
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-11-10 13:46:08 -0700

    Added some messages and some more outlets. I also added images.

--- JeremyS_CSP/Controller/GlobalImpactViewController.swift
+++ JeremyS_CSP/Controller/GlobalImpactViewController.swift
@@ -11,10 +11,22 @@ import UIKit
 public class GlobalImpactViewController: UIViewController {
     
     @IBOutlet weak var imageImpact: UIImageView!
+    @IBOutlet weak var imageImpactRelated: UIImageView!
+    @IBOutlet weak var whyLabel: UILabel!
+    @IBOutlet weak var personalConnection: UILabel!
+    
     
     private func setupGlobalImpact() -> Void {
+        imageImpact.image = UIImage (named: "ok")
+        imageImpactRelated.image = UIImage (named:"right")
+        
+        whyLabel.text =  ("The reason this global problem needs to be addressed is because human rights have been violated for a long time. Throughout history we have underestimated others and we have kept them as slaves, and if not slaves, as those who could not handle what we could do. It has been a while and a lot of the world has progressed, but a lot of parts in the world have not. It is important because it has been forever. We must help minimize it completely. ")
+        
+        personalConnection.text = ("This affects me because personally I don't like seeing people being treated badly or poorly for extremely judgemental reasons. I dislike that feeling.")
         
     }
+    
+    
 
     public override func viewDidLoad() {
         super.viewDidLoad()
