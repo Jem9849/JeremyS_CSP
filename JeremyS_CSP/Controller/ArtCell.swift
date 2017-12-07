59cb7d251d37321f103afa398f85480f201a4784
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-12-07 12:53:45 -0700

    Trying to fix my creativiity controller and finish it, I also created a custom collection view cell class.

---
+++ JeremyS_CSP/Controller/ArtCell.swift
@@ -0,0 +1,21 @@
+//
+//  ArtCell.swift
+//  JeremyS_CSP
+//
+//  Created by Saysanasy, Jeremy on 12/7/17.
+//  Copyright © 2017 Saysanasy, Jeremy. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell : UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var imageName: UILabel!
+
+    
+    
+    
+    
+}
--- JeremyS_CSP/Controller/CreativityViewController.swift
+++ JeremyS_CSP/Controller/CreativityViewController.swift
@@ -8,24 +8,35 @@
 
 import UIKit
 
-public class CreativityViewController: UIViewController
+public class CreativityViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
-    private lazy var art
+    private let reuseIdentifier = "artIdentifier"
+    private let sectionInsets = UIEdgeInsets(top: 50.0,
+                                             left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow : CGFloat = 3
     
-    
-    
-    
-    
-    
-    
-    
-    
+    private lazy var artSelection : [UIImage?] =
+    {
+        return [
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d"),
+            UIImage(named: "d")
+        ]
+    }()
     
     var largePhotoIndexPath: IndexPath?
     {
         didSet
         {
-            var indexPaths = [indexPath]{}
+            var indexPaths = [indexPath]()
             if let largePhotoIndexPath = largePhotoIndexPath
             {
                 indexPaths.append(largePhotoIndexPath)
@@ -36,18 +47,18 @@ public class CreativityViewController: UIViewController
             }
             
             collectionView?.performBatchUpdates(
-            {
+                {
                 self.collectionView?.reloadItems(at: indexPaths)
             })
-        {
-            completed in
-            
-            if let largePhotoIndexPath = self.largePhotoIndexPath
             {
+                completed in
+            
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
                 self.collectionView?.scrollToItem(at: largePhotoIndexPath,
                                                   at: .centeredVertically,
-            }                                     animated: true)
-         }
+                }                                 animated: true)
+            }
        }
     }
 
