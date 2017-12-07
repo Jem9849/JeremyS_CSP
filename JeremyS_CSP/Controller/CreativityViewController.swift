0d1fc07034ed3d6f4e45236b432afc39d343806b
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-12-07 12:33:31 -0700

    Trying to catch up in my code. There is a lot of errors right now.

--- JeremyS_CSP/Controller/CreativityViewController.swift
+++ JeremyS_CSP/Controller/CreativityViewController.swift
@@ -8,15 +8,130 @@
 
 import UIKit
 
-public class CreativityViewController: UIViewController {
+public class CreativityViewController: UIViewController
+{
+    private lazy var art
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [indexPath]{}
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+            {
+                self.collectionView?.reloadItems(at: indexPaths)
+            })
+        {
+            completed in
+            
+            if let largePhotoIndexPath = self.largePhotoIndexPath
+            {
+                self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                  at: .centeredVertically,
+            }                                     animated: true)
+         }
+       }
+    }
 
-    public override func viewDidLoad() {
+    //MARK: Lifecycle methods
+    override public func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    public override func didReceiveMemoryWarning() {
+    
+    //MARK: -UICollectionView methods
+    
+    override public func numberOfSections(in collectionView: UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    override public func collectioniew(_ collectionView: UICollectionView,
+                                       numberOfItemsInSection section: Int) -> Int
+    {
+        return artSelection.count
+    }
+    
+    override public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCell
+        
+        artCell.backgroundColor = .purple
+        artCell.imageView.image = artSelection[indexPath.row]
+        artCell.imageName.text = "Art"
+        
+        return artCell
+    }
+    
+    //MARK: - Delegate methods
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath == largePhotoIndexPath
+        {
+            let art = artSelection[indexPath.row]
+            let size = collectionView.bounds.size
+            let widthScale = (size.width / art!.size.width) * CGFloat(0.80)
+            let largeSize = CGSize(width: art!.size.width * widthScale, height: art!.size.height * widthScale)
+            
+            return largeSize
+        }
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return sectionInsets
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return sectionInsets.left
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override public func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
