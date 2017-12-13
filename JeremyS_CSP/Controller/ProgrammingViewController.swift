8d49f2c36413c13a0f658b4273a0f72a6188d4a1
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-12-13 12:19:11 -0700

    Added code that does things for the gameView and the scene.

--- JeremyS_CSP/Controller/ProgrammingViewController.swift
+++ JeremyS_CSP/Controller/ProgrammingViewController.swift
@@ -7,29 +7,24 @@
 //
 
 import UIKit
+import SpriteKit
 
-public class ProgrammingViewController: UIViewController {
+public class ProgrammingViewController: UIViewController
+{
 
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    public override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    //MARK: UI Components
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override public func viewDidLoad() -> Void
+    {
+    super.viewDidLoad()
+    let gameScene = StartScene(size: view.bounds.size)
+    let gameView = view as! SKView
+    
+    gameView.showsFPS = true
+    gameView.showsNodeCount = true
+    gameView.ignoresSiblingOrder = true
+    
+    gameScene.scaleMode = .resizeFill
+    gameView.presentScene(gameScene)
     }
-    */
-
 }
