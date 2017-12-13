f687648e1b21378b496c34c868a17c9c580b6a69
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-12-13 13:51:34 -0700

    Removed a problemsome method and added the physics for the bullet of invader.

--- JeremyS_CSP/Model/InvaderLaser.swift
+++ JeremyS_CSP/Model/InvaderLaser.swift
@@ -15,16 +15,18 @@ public class InvaderBullet: Projectile
     {
         super.init(imageName: imageName, bulletSound: bulletSound)
         
+        self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.categoryBitMask = CollisionCategories.InvaderBullet
+        self.physicsBody?.contactTestBitMask = CollisionCategories.Player
+        self.physicsBody?.collisionBitMask = 0x0
+        
     }
     
     required public init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
-    }
-    
-    public func fireBullet(scene: SKScene) -> Void
-    {
-        
     }
     
 }
